29a02ff1812bc853d0baadf1588018684c7f071e
danish-ali1 <datali4444@gmail.com>
2025-11-05 20:44:58 +0530

    Third commit

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,11 +2,7 @@ import mongoose from 'mongoose';
 
 export const connectDb= async () =>{
     try {
-       const conn= await mongoose.connect(process.env.MONGO_URI,{
-           serverSelection: {
-               family: 4 // This is the fix!
-           }
-       });
+       const conn= await mongoose.connect(process.env.MONGO_URI );
        console.log(`MongoDB connected to ${conn.connection.host}`);
     } catch (error) {
         console.error("MongoDB connection error:", error);
